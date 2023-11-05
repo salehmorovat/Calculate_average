75da122c18286c328e9d7c8bd063126c219d106f
salehmorovat <126867033+salehmorovat@users.noreply.github.com>
2023-11-06 00:21:59 +0330

    Calculate the average
    Calculate the average

---
+++ Calculate_average.py
@@ -0,0 +1,52 @@
+adabiat = int (input( " What was your grade in adabiat ? " ))
+barname_nevisi = int (input( " What was your grade in barname nevisi ? " ))
+akhlagh_eslami = int (input( " What was your grade in akhlagh eslami ? " ))
+riazi = int (input( " What was your grade in riazi ? " ))
+kar_afarini = int (input( " What was your grade in kar afarini ? " ))
+paygah_dade = int (input( " What was your grade in paygah dade ? " ))
+tejarat_electronici = int (input( " What was your grade in tejarat electronici ? " ))
+tarbiat_badani = int (input( " What was your grade in tarbiat badani ? " ))
+
+
+
+nomre_kol = ( riazi +  adabiat + barname_nevisi + akhlagh_eslami + kar_afarini + paygah_dade + tejarat_electronici + tarbiat_badani )
+
+
+Moaddel = ( nomre_kol ) /10
+
+print ("moaddel kol shoma " ,Moaddel, "ast!" )
+
+
+while Moaddel != 20 :
+    print ( "You fail!" )
+    break
+
+
+
+adabiat = int (input( " What was your grade in adabiat ? " ))
+barname_nevisi = int (input( " What was your grade in barname nevisi ? " ))
+akhlagh_eslami = int (input( " What was your grade in akhlagh eslami ? " ))
+riazi = int (input( " What was your grade in riazi ? " ))
+kar_afarini = int (input( " What was your grade in kar afarini ? " ))
+paygah_dade = int (input( " What was your grade in paygah dade ? " ))
+tejarat_electronici = int (input( " What was your grade in tejarat electronici ? " ))
+tarbiat_badani = int (input( " What was your grade in tarbiat badani ? " ))
+
+
+
+nomre_kol = ( riazi + adabiat + barname_nevisi + akhlagh_eslami + kar_afarini + paygah_dade + tejarat_electronici + tarbiat_badani )
+
+
+
+Moaddel = ( nomre_kol ) /10
+
+print ("moaddel kol shoma " ,Moaddel, "ast!" )
+while Moaddel == 20 :
+    print ( " happy neww bike ")
+
+
+
+
+
+
+    
